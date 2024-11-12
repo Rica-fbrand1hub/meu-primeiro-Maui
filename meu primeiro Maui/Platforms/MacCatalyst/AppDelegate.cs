5317d06754f3e06242141548b1d5a16c46e72217
ricaw <ricaw@RWORKERDEIA>
2024-11-12 15:01:51 -0300

    Adicionar arquivos de projeto.

---
+++ meu primeiro Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace meu_primeiro_Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
